68c73ca3409fedf5dff80691c582e0e475c2d48f
Anna Mefford <annamefford@gmail.com>
2016-10-15 20:12:59 -0600

    colorBox creation

--- artmaker.js
+++ artmaker.js
@@ -1,30 +1,79 @@
 "use strict";
 
-//var grid = document.getElementById("line1");
-//console.log(grid);
-var gridItem = document.getElementsByClassName("gridBox");
-//console.log(gridItem);
+(function() {
 
-var colorContainer = document.querySelector(".colorContainer");
-console.log(colorContainer);
+  var colors = [
+    '#FF3855',
+    '#FFAA1D',
+    '#FFF700',
+    '#299617',
+    '#2243B6',
+    '#5DADEC',
+    '#5946B2',
+    '#9C51B6',
+    '#A83731',
+    '#AF6E4D',
+    '#1B1B1B',
+    '#BFAFB2',
+    '#FF5470',
+    '#FF7A00',
+    '#87FF2A',
+    '#FF007C',
+    '#8A496B',
+    '#4570E6',
+    '#DA2C43',
+    '#44D7A8',
+    'white',
+  ];
+  var brushColor;
+  setBrushColor('white');
 
-function addClick(){
-  var i;
-  for (i=0; i<gridItem.length; i++){
-    //console.log(gridItem[i]);
-    gridItem[i].addEventListener("click", function(){
-      this.style.backgroundColor = brushColor;
+  var colorContainer = document.querySelector(".colorContainer");
+  var gridItem = document.getElementsByClassName("gridBox");
+
+  // (function buildGrid(){
+  //   var i;
+  //   var gridContainerNode = document.querySelector(".gridContainer");
+  //   console.log(gridContainerNode);
+  //
+  //   // for (i=0; i<100; i++){
+  //   //
+  //   // }
+  //
+  //
+  // })();
+
+
+  (function buildColorPallette() {
+    colors.forEach(function(colorId) {
+      var colorContainerNode = document.getElementById("colorContainer");
+      var colorItem = document.createElement("div");
+      colorItem.style.backgroundColor = colorId; // Add backgroundColor to element via style attribute
+      colorItem.className += 'colorBox'; // Adding a className
+      colorItem.dataset.colorId = colorId;
+      colorContainerNode.appendChild(colorItem); // Adding to a parent
     });
+  })();
+
+  (function addClick(){
+    var i;
+    var clickHandler = function(){
+      this.style.backgroundColor = brushColor;
+    };
+
+    for (i=0; i<gridItem.length; i++){
+      gridItem[i].addEventListener("click", clickHandler);
+    }
+  })();
+
+  colorContainer.addEventListener("click", function(){
+    var colorBox = event.target;
+    setBrushColor(colorBox.dataset.colorId);
+    return brushColor;
+  });
+
+  function setBrushColor(colorId) {
+    brushColor = colorId;
+    document.querySelector(".brushColor").style.backgroundColor = colorId;
   }
-}
-console.log(addClick());
-
-var brushColor;
-
-colorContainer.addEventListener("click", function(){
-  brushColor = event.target.getAttribute('id');
-  document.querySelector(".brushColor").style.backgroundColor = brushColor;
-  console.log(brushColor);
-  return brushColor;
-});
-console.log(brushColor);
+})();
